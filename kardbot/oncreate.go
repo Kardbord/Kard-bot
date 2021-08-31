5f6f656b622ed544d46be789df680bf26b764a5f
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2021-08-30 23:57:54 -0600

    Removed unnecessary onmessage.go file

--- kardbot/oncreate.go
+++ kardbot/oncreate.go
@@ -32,6 +32,21 @@ var (
 	}
 )
 
+type onCreateHandlerf = func(*discordgo.Session, *discordgo.MessageCreate)
+
+type onCreateHandler struct {
+	handler onCreateHandlerf
+	help    string
+}
+
+// Any callbacks that happen onMessageCreate belong in this list.
+// It is the duty of each individual function to decide whether or not to run.
+// These callbacks must be able to safely execute asynchronously.
+var onCreateHandlers = [...]onCreateHandler{
+	{greeting, fmt.Sprintf("Returns your salutations when you greet the bot with %v", greetings)},
+	{farewell, fmt.Sprintf("Returns your valediction when you tell the bot %v", farewells)},
+}
+
 func fromSelf(s *discordgo.Session, m *discordgo.MessageCreate) bool {
 	return m.Author.ID == s.State.User.ID
 }
