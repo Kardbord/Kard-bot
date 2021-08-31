9ec0c66a97e8d5397d2101b736b47189ac107d0b
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2021-08-30 22:05:27 -0600

    Greetings/farewells are slightly more dynamic

--- kardbot/onmessage/oncreate.go
+++ kardbot/onmessage/oncreate.go
@@ -2,6 +2,10 @@ package onmessage
 
 import (
   "fmt"
+  "log"
+  "math/rand"
+  "regexp"
+  "strings"
 
   "github.com/bwmarrin/discordgo"
 )
@@ -15,8 +19,29 @@ func greeting(s *discordgo.Session, m *discordgo.MessageCreate) {
     return
   }
 
-  if m.Content == fmt.Sprintf("Hello %s!", s.State.User.Username) {
-    s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Hello %s!", m.Author.Username))
+  // Possible greetings the bot will respond to
+  // TODO: make these configurable
+  greetings := []string{
+    "Hello",
+    "Hi",
+    "Greetings",
+		"Salutations",
+  }
+  greetingGroup := buildRegexAltGroup(greetings)
+
+  matched, err := regexp.MatchString(
+    fmt.Sprintf("^(?i)%s %s[!.]*$", greetingGroup, buildBotNameRegexp(s.State.User.Username)),
+    m.Content,
+  )
+  if err != nil {
+    log.Println("Regex error: ", err)
+    return
+  }
+  if matched {
+    s.ChannelMessageSend(
+      m.ChannelID,
+      fmt.Sprintf("%s %s!", greetings[rand.Intn(len(greetings))], m.Author.Username),
+    )
   }
 }
 
@@ -25,7 +50,63 @@ func farewell(s *discordgo.Session, m *discordgo.MessageCreate) {
     return
   }
 
-  if m.Content == fmt.Sprintf("Goodbye %s!", s.State.User.Username) {
-    s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Goodbye %s!", m.Author.Username))
+  // Possible farewells the bot will respond to
+  // TODO: make these configurable
+  farewells := []string{
+    "Goodbye",
+    "Farewell",
+    "So long",
+		"Bye",
+		"See you",
+		"See ya",
   }
+  farewellGroup := buildRegexAltGroup(farewells)
+
+  matched, err := regexp.MatchString(
+    fmt.Sprintf("^(?i)%s %s[!.]*$", farewellGroup, buildBotNameRegexp(s.State.User.Username)),
+    m.Content,
+  )
+  if err != nil {
+    log.Println("Regex error: ", err)
+    return
+  }
+  if matched {
+    s.ChannelMessageSend(
+      m.ChannelID,
+      fmt.Sprintf("%s %s!", farewells[rand.Intn(len(farewells))], m.Author.Username),
+    )
+  }
+}
+
+// Some characters are optional when matching the bot name.
+// This function returns a regexp string to appropriately
+// match the bot name, including any optional characters.
+func buildBotNameRegexp(botName string) string {
+  // TODO: make these configurable
+  optionalRunes := []rune{
+    '-',
+    '_',
+  }
+  botNameExp := botName
+  for _, r := range optionalRunes {
+    botNameExp = strings.ReplaceAll(botNameExp, string(r), fmt.Sprintf("%s?", string(r)))
+  }
+  log.Println("Built bot exp=", botNameExp)
+  return botNameExp
+}
+
+// Returns a regexp alternate group of the provided
+// strings. For example, input of [a ,b] would result
+// in a return value of "(a|b)".
+func buildRegexAltGroup(alts []string) string {
+  altGroup := "("
+  for i, alt := range alts {
+    altGroup += alt
+    if i+1 < len(alts) {
+      altGroup += "|"
+    }
+  }
+  altGroup += ")"
+  log.Println("Built altgroup=", altGroup)
+  return altGroup
 }
